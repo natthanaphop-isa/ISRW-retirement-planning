19e364a953609a3d58078abeb2681ef46e70d0e8
natthanaphop-isa <58567478+natthanaphop-isa@users.noreply.github.com>
2024-11-27 14:23:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -140,8 +140,8 @@ fig, df = retirement_simulation(
 final_fund_balance = df.iloc[-1]['Fund Balance']
 if final_fund_balance > 0:
     status = "แผนเกษียณสำเร็จ ✅"
-    recommendation = (f"เงินทุนเกษียณของคุณมากพอต่อค่าใช้จ่ายหลังเกษียณ รวมเงินเฟ้อ {inflation_rate*100:.1f}% ต่อปีจนสิ้นอายุขัย",
-                        f"และมีมรดกหลังสิ้นอายุขัย: ฿{inheritance:,.0f}")
+    recommendation = (f"เงินทุนเกษียณของคุณมากพอต่อค่าใช้จ่ายหลังเกษียณ รวมเงินเฟ้อ {inflation_rate*100:.1f}% ต่อปีจนสิ้นอายุขัย")
+                        #f"และมีมรดกหลังสิ้นอายุขัย: ฿{inheritance:,.0f}")
     box_color = "#D4EDDA"  # Green box color for success
     text_color = "#155724"  # Dark green text for success
 else:
